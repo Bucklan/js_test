5e547402e712802df6966e39ad4cbca3f4062a8e
Bucklan <as1kenjebaev@gmail.com>
2023-10-20 19:29:22 +0600

    Add basic structure for TodoList
    The basic HTML, CSS, and JavaScript files for a TodoList application were created.
    The index.html file was added with all the necessary components for a TodoList including the elements for display task list, and checkboxes for marking tasks as done or important.
    The main.js file was created to handle the basic operations for the list.
    The style.css file was added to give a general style for the application.
    These changes establish the foundational structure and design for the project.

---
+++ todolist/main.js
@@ -0,0 +1,22 @@
+let todoList = [
+    {
+        id: 1,
+        title: 'name task 1',
+        isDone: false,
+        isImportant: false,
+    },
+    {
+        id: 2,
+        title: 'name task 2',
+        isDone: false,
+        isImportant: false,
+    },
+    {
+        id: 3,
+        title: 'name task 3',
+        isDone: false,
+        isImportant: false,
+    },
+];
+
+let list = document.querySelector('.todo__list');
