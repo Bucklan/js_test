2b8a47567b3170dccbe22155275cd6927d8de285
Bucklan <as1kenjebaev@gmail.com>
2023-10-22 16:57:28 +0600

    Add input validation and importance feature to todo list
    In this change, two new features were added to the Todo List. Firstly, the `<input>` in `index.html` was changed to require a value before submitting the form. This prevents users from adding empty todos.
    
    Additional functionality was also added in `main.js` to mark a todo item as important. The importance status influences the color of the todo item title, helping the user to easily identify important tasks. This is achieved through adding an 'isImportant' property to each item and controlling its status with an onclick event.

--- todolist/main.js
+++ todolist/main.js
@@ -21,18 +21,20 @@ let todoList = [
 
 let list = document.querySelector('.todo__list');
 let form = document.querySelector('.todo__form');
+let todoBtnDelete;
 const showList = () => {
     todoList.forEach((item, idx) => {
-        list.innerHTML += '<li class="todo__item">' +
-            '                <span class="todo__item-title">' + item.title + '</span>' +
-            '                <div class="todo__item-buttons">' +
-            '                    <button class="todo__item-button done">Done</button>' +
-            '                    <button class="todo__item-button important">Important</button>' +
-            '                    <button class="todo__item-button delete" >Delete</button>' +
-            '                </div>' +
-            '            </li>'
+        list.innerHTML += `<li class="todo__item">
+                <span style="color: ${item.isImportant ? 'red' : ''}" class="todo__item-title">${item.title}</span>
+                <div class="todo__item-buttons">
+                    <button class="todo__item-button done">Done</button>
+                    <button class="todo__item-button important">Important</button>
+                    <button class="todo__item-button delete">Delete</button>
+                </div>
+            </li>`
     });
-    let todoBtnDelete = document.querySelectorAll('.delete');
+    todoBtnDelete = document.querySelectorAll('.delete');
+    let todoImportant = document.querySelectorAll('.important');
 
     todoBtnDelete.forEach((item, idx) => {
         item.addEventListener('click', (evt) => {
@@ -43,10 +45,24 @@ const showList = () => {
             showList()
         });
     });
+
+    todoImportant.forEach((item, idx) => {
+        item.addEventListener('click', (evt) => {
+            todoList = todoList.map((el, index) => {
+                if (index === idx) {
+                    return {...el, isImportant: !el.isImportant}
+                }
+                return el
+            })
+            list.innerHTML = ''
+            showList();
+        })
+
+    })
 }
 form.addEventListener('submit', (evt) => {
     evt.preventDefault();
-    todoList = [...todoList,{
+    todoList = [...todoList, {
         id: todoList[todoList.length - 1].id + 1,
         title: evt.target[0].value,
         isDone: false,
