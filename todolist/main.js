b09035a5fe65fdc0a3bb3c1336bb952b401463a0
Bucklan <as1kenjebaev@gmail.com>
2023-10-22 16:09:31 +0600

    Add 'delete' event listener and form submission handling
    In this commit, I added functionality for deleting todo items and submitting new items. Now, when users click the 'delete' button on a todo item, it will be removed from the todoList and no longer display in the list. I added an eventListener for this action on every 'delete' button. Additionally, when the form to submit a new todo item is completed, the new item is added to the todoList and displayed in the list. Form submission is prevented to avoid page refresh.

--- todolist/main.js
+++ todolist/main.js
@@ -20,18 +20,41 @@ let todoList = [
 ];
 
 let list = document.querySelector('.todo__list');
-
+let form = document.querySelector('.todo__form');
 const showList = () => {
-    todoList.forEach((item,idx) =>{
-        list.innerHTML+= '<li class="todo__item">' +
-            '                <span class="todo__item-title">'+ item.title+'</span>' +
+    todoList.forEach((item, idx) => {
+        list.innerHTML += '<li class="todo__item">' +
+            '                <span class="todo__item-title">' + item.title + '</span>' +
             '                <div class="todo__item-buttons">' +
             '                    <button class="todo__item-button done">Done</button>' +
             '                    <button class="todo__item-button important">Important</button>' +
-            '                    <button class="todo__item-button delete">Delete</button>' +
+            '                    <button class="todo__item-button delete" >Delete</button>' +
             '                </div>' +
             '            </li>'
-    })
+    });
+    let todoBtnDelete = document.querySelectorAll('.delete');
+
+    todoBtnDelete.forEach((item, idx) => {
+        item.addEventListener('click', (evt) => {
+            todoList = todoList.filter((el, index) => {
+                return index !== idx
+            });
+            list.innerHTML = ''
+            showList()
+        });
+    });
 }
+form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    todoList = [...todoList,{
+        id: todoList[todoList.length - 1].id + 1,
+        title: evt.target[0].value,
+        isDone: false,
+        isImportant: false,
+    }];
+    evt.target[0].value = ''
+    list.innerHTML = ''
+    showList()
+});
 
 showList()
